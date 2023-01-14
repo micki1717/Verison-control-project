a0787ad43e7160cbde2bfdf9ea433448b8899dfd
MeiJia <mickihe1017@gmail.com>
2023-01-13 19:51:31 -0500

    Change DS and comments

--- bikeshare.py
+++ bikeshare.py
@@ -1,3 +1,4 @@
+# Change DS and comments 
 import time
 import pandas as pd
 import numpy as np
@@ -8,12 +9,12 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Let's ask user to specify a city, month, and day to analyze.
 
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) CITY - name of the city to analyze 
+        (str) MONTH - name of the month to filter by, or "all" to apply no month filter
+        (str) DAY - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
